8860ef6e38276baf39085f4f8052f8db3d7d296c
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-22 18:03:50 -0500

    attempting to create CRUD functionality

--- backend/controllers/gifs.js
+++ backend/controllers/gifs.js
@@ -14,3 +14,29 @@ router.get('/', (req, res) => {
     })
 })
 
+// // id param
+// router.get('/:id', (req, res) => {
+//     let id = req.params.id
+
+// })
+
+// create
+router.post('/', (req, res) => {
+    Gif.create(req.body.gif)
+        .then(gif => {
+            // res.send ??
+            res.send
+        })
+})
+
+// id param
+router.put('/:id', (req, res) => {
+    Gif.findOneAndUpdate({id: req.params.id})    
+})
+
+// id param
+router.delete('/:id', (req, res) => {
+
+})
+
+// export router
