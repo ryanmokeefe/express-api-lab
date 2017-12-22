0816f5456a46eb5c09a8c91e857dc1d4e4213852
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-22 14:27:10 -0500

    added seeds and seedData

---
+++ gifs/db/seeds.js
@@ -0,0 +1,11 @@
+const Gif = require('./schema')
+const seedData = require('./seeds.json')
+
+Gif.remove({})
+    .then(() => {
+        return Gif.collection.insert(seedData)
+    })
+    .then(() => {
+        process.exit()
+    })
+
